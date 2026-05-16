c1d59a5d0f9ab2e1b1ef329ccf92575f9e48b688
avishek93820 <avishek93820@gmail.com>
2026-05-16 20:19:04 +0530

    Update ParticleBackground.tsx

--- src/components/ui/ParticleBackground.tsx
+++ src/components/ui/ParticleBackground.tsx
@@ -1,7 +1,4 @@
-import Particles, { initParticlesEngine } from '@tsparticles/react';
-import { loadSlim } from '@tsparticles/slim';
-import { useEffect, useMemo, useState } from 'react';
-import type { ISourceOptions } from '@tsparticles/engine';
+
 
 
 export function ParticleBackground() {
